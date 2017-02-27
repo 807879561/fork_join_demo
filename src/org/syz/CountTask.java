e55472cd27776019118595b5fa17d256fb6b0a6a
shiyizhen <shiyz@miqtech.com>
2017-02-27 15:29:23 +0800

    init

---
+++ src/org/syz/CountTask.java
@@ -0,0 +1,53 @@
+package org.syz;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+public class CountTask extends RecursiveTask<Integer> {
+	private static final long serialVersionUID = 7287149829993735705L;
+	private static final int THRESHOLD = 10;//阈值
+	private int start;
+	private int end;
+
+	public CountTask(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+
+	@Override
+	protected Integer compute() {
+		int sum = 0;
+		boolean canCompute = (end - start) <= THRESHOLD;
+		if (canCompute) {
+			for (int i = start; i <= end; i++) {
+				sum += i;
+			}
+		} else {
+			int middle = (start + end) / 2;
+			CountTask leftTask = new CountTask(start, middle);
+			CountTask rightTask = new CountTask(middle + 1, end);
+			leftTask.fork();
+			rightTask.fork();
+			int leftResult = leftTask.join();
+			int rightResult = rightTask.join();
+			sum = leftResult + rightResult;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		ForkJoinPool forkJoinPool = new ForkJoinPool();//ForkJoinPool由ForkJoinTask数组和ForkJoinWorkerThread数组组成，ForkJoinTask数组负责存放程序提交给ForkJoinPool的任务，而ForkJoinWorkerThread数组负责执行这些任务。
+		CountTask task = new CountTask(1, 1000);
+		Future<Integer> result = forkJoinPool.submit(task);
+		try {
+			System.out.println(result.get());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
